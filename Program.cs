092325f72adf630d2ae2a33550724a84234c5dc6
Tania Shulga <tshulha@pearlsolutions.com>
2019-09-11 13:43:08 -0500

    set app vars

--- Program.cs
+++ Program.cs
@@ -10,9 +10,15 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            string appName = "WebLorat";
+            string appVersion = "1.0.0";
+            string appAuthor = "Loratadin Legacy";
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
             string name = "Loratadin Legacy";
             int age = 35;
             Console.WriteLine("{0} is {1}", name, age);
+
         }
     }
 }
