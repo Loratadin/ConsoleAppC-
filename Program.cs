49f908079db088530c62d1866c93694b576fc51b
Tania Shulga <tshulha@pearlsolutions.com>
2019-09-25 09:54:50 -0500

    interact with the user

--- Program.cs
+++ Program.cs
@@ -14,10 +14,15 @@ namespace ConsoleApp
             string appVersion = "1.0.0";
             string appAuthor = "Loratadin Legacy";
 
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
             string name = "Loratadin Legacy";
             int age = 35;
+            Console.ResetColor();
             Console.WriteLine("{0} is {1}", name, age);
+            Console.WriteLine("What is your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello {0}, let's play a game", input);
 
         }
     }
