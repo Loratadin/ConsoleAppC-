5e67b1265cf7061c4ec0a8480a204dce17821ca5
Tania Shulga <tshulha@pearlsolutions.com>
2019-09-25 11:19:40 -0500

    add continue / quit option

--- Program.cs
+++ Program.cs
@@ -23,31 +23,41 @@ namespace ConsoleApp
             Console.WriteLine("What is your name?");
             string inputName = Console.ReadLine();
             Console.WriteLine("Hello {0}, let's play a game", inputName);
-            //int correctNumber = 7;
-            Random random = new Random();
-            int correctNumber = random.Next(1, 10);
-            int guess = 0;
-            Console.WriteLine("Guess a number between 1 and 10");
-            while (guess != correctNumber)
-            {
-                string input = Console.ReadLine();
-                if(!int.TryParse(input, out guess)) {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Please, enter an actual number");
-                    Console.ResetColor();
-                    continue;
-                }
-                guess = Int32.Parse(input);
-                if (guess != correctNumber)
+            while (true) { 
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+                int guess = 0;
+                Console.WriteLine("Guess a number between 1 and 10");
+                while (guess != correctNumber)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Wrong number, please try again");
-                    Console.ResetColor();
+                    string input = Console.ReadLine();
+                    if(!int.TryParse(input, out guess)) {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please, enter an actual number");
+                        Console.ResetColor();
+                        continue;
+                    }
+                    guess = Int32.Parse(input);
+                    if (guess != correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong number, please try again");
+                        Console.ResetColor();
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("You are CORRECT!!!");
+                Console.ResetColor();
+                Console.WriteLine("Play again? [Y or N]");
+                string answer = Console.ReadLine().ToUpper();
+                if (answer == "Y") {
+                    continue;
+                } else if (answer == "N") {
+                    return;
+                } else {
+                    return;
                 }
             }
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("You are CORRECT!!!");
-            Console.ResetColor();
         }
     }
 }
